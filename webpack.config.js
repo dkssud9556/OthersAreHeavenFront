384fb000e7e4fc25d70918621fe1bf5f55591d6b
김대웅 <dkssud9556@dsm.hs.kr>
2021-03-30 15:49:04 +0900

    [ADD] project base

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'none',
+  entry: {
+    app: path.join(__dirname, 'src', 'index.jsx')
+  },
+  target: 'web',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      inject: 'body',
+      template: path.resolve(__dirname, 'index.html')
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    inline: true,
+    hot: true,
+    host: 'localhost',
+    port: 3000,
+    open: 'whale'
+  }
+}
