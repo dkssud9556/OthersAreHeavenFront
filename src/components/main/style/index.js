b701f51e0d7edeb0eb61c9e3a39782e2868a1463
김대웅 <dkssud9556@dsm.hs.kr>
2021-04-16 09:52:49 +0900

    Fix some css

--- src/components/main/style/index.js
+++ src/components/main/style/index.js
@@ -10,42 +10,42 @@ export const Container = styled.div`
 `;
 
 export const TitleContainer = styled.div`
-  padding-top: 30px;
-  padding-left: 30px;
-  width: 500px;
+  padding-top: 50px;
+  padding-left: 50px;
+  width: 600px;
 `;
 
 export const Title = styled.h1`
   padding-top: 15px;
-  font-size: 65px;
+  font-size: 100px;
   font-weight: bold;
   color: black;
 `;
 
 export const Description = styled.div`
-  font-size: 35px;
+  font-size: 50px;
   padding-top: 20px;
 `;
 
 export const AccountContainer = styled.div`
-  width: 350px;
+  width: 600px;
   height: 80vh;
   background: rgba(255, 255, 255, 0.8);
   border-radius: 30px;
   margin-top: 10vh;
-  margin-left: 25vh;
+  margin-left: 45vh;
   display: flex;
   align-items: center;
   flex-direction: column;
 `;
 
 export const AccountTitle = styled.h1`
-  font-size: 45px;
+  font-size: 65px;
   font-weight: bold;
 `;
 
 export const AccountTitleContainer = styled.div`
-  margin-top: 40px;
+  margin-top: 55px;
 `;
 
 export const AccountInputContainer = styled.div`
@@ -54,10 +54,12 @@ export const AccountInputContainer = styled.div`
 `;
 
 export const AccountInput = styled.input`
-  margin-top: 25px;
-  border-radius: 24px;
+  font-size: 25px;
+  margin-top: 40px;
+  border-radius: 32px;
   padding: 10px;
   width: 100%;
+  height: 65px;
   box-sizing: border-box;
   outline: none;
   background: white;
@@ -70,11 +72,11 @@ export const AccountButtonContainer = styled.div`
 `;
 
 export const AccountButton = styled.button`
-  border-radius: 24px;
-  height: 40px;
+  border-radius: 32px;
+  height: 60px;
   box-sizing: border-box;
   width: 100%;
-  font-size: 20px;
+  font-size: 30px;
   outline: none;
   background: white;
   border: 1px solid #707070;
@@ -85,7 +87,7 @@ export const AccountButton = styled.button`
 
 export const AccountModeChangeButton = styled.div`
   margin-top: 10px;
-  font-size: 12px;
+  font-size: 20px;
   &:hover {
     cursor: pointer;
     color: blue;
@@ -93,12 +95,13 @@ export const AccountModeChangeButton = styled.div`
 `;
 
 export const EmailSendButton = styled.button`
-  margin-top: 25px;
-  border-radius: 24px;
+  margin-top: 40px;
+  border-radius: 32px;
   width: 40%;
   outline: none;
   background: white;
   border: 1px solid #707070;
+  font-size: 20px;
   &:hover {
     cursor: pointer;
   }
@@ -111,4 +114,10 @@ export const EmailVerificationContainer = styled.div`
 
 export const EmptySpaceBetweenInputAndButton = styled.div`
   width: 5%;
+`;
+
+export const SubmitForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
