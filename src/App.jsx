171e1bd058950a490a2dbf46fd9971b9543a85ac
김대웅 <dkssud9556@dsm.hs.kr>
2021-04-30 09:24:14 +0900

    [ADD] app.jsx store provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { Provider } from "react-redux";
 import GlobalStyle from "./globalStyle";
 import Main from "./components/main/Main";
+import store from "./modules/store";
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <Main />
-    </>
+    </Provider>
   );
 }
