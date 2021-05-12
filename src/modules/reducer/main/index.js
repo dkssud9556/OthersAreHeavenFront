7027edf89d416e6e6a7a60e6715159323ae58863
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-12 17:02:00 +0900

    [ADD] main reducer

---
+++ src/modules/reducer/main/index.js
@@ -0,0 +1,21 @@
+import { LOGIN_MODE } from "../../action/main";
+
+const initState = {
+  isLoginMode: false,
+};
+
+const MainReducer = (state = initState, action) => {
+  switch (action.type) {
+    case LOGIN_MODE: {
+      return {
+        ...state,
+        isLoginMode: action.payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default MainReducer;
