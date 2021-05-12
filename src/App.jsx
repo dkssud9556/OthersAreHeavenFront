3a8cd40e743e2c42e9aafea34ba459bfc4e95b67
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-12 16:57:08 +0900

    [ADD] Root Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
 import GlobalStyle from "./globalStyle";
-import Main from "./components/main/Main";
 import store from "./modules/store";
+import RootRouter from "./route";
 
 export default function App() {
   return (
     <Provider store={store}>
       <GlobalStyle />
-      <Main />
+      <RootRouter />
     </Provider>
   );
 }
