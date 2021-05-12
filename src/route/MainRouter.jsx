3e255038a85684d4a39be175dbecbc9faa917cf5
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-12 16:59:02 +0900

    [ADD] Main Router

--- src/route/MainRouter.jsx
+++ src/route/MainRouter.jsx
@@ -1,3 +1,15 @@
-const MainRouter = () => {};
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import MainContainer from "../containers/main/MainContainer";
+
+const MainRouter = () => {
+  return (
+    <>
+      <Switch>
+        <Route path="/" component={MainContainer} />
+      </Switch>
+    </>
+  );
+};
 
 export default MainRouter;
