dd24c57713a777a06ca3d8e637a6751752d1879d
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-24 23:33:19 +0900

    [ADD] main reducer

--- src/modules/reducer/main/index.js
+++ src/modules/reducer/main/index.js
@@ -1,17 +1,22 @@
 import { LOGIN_MODE } from "../../action/main";
+import { GO_TO_MATCH } from "../../action/main/interface";
 
 const initState = {
   isLoginMode: false,
+  token: localStorage.getItem("token"),
 };
 
 const MainReducer = (state = initState, action) => {
   switch (action.type) {
-    case LOGIN_MODE: {
+    case LOGIN_MODE:
       return {
         ...state,
         isLoginMode: action.payload,
       };
-    }
+    case GO_TO_MATCH:
+      return {
+        ...state,
+      };
     default: {
       return state;
     }
