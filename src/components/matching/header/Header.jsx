9823b99a6689d157b8c22713e1d36a6ae4934f4b
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-24 23:28:21 +0900

    [ADD] match header

---
+++ src/components/matching/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "../style";
+
+const Header = () => {
+  return (
+    <S.Header>
+      <S.LogoutAnchor>로그아웃</S.LogoutAnchor>
+    </S.Header>
+  );
+};
+
+export default Header;
