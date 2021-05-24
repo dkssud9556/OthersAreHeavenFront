0c1567960cc9a964ca80a4f03ea43b87cfde5a94
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-24 23:27:46 +0900

    [FIX] Login.jsx

--- src/components/main/login/Login.jsx
+++ src/components/main/login/Login.jsx
@@ -1,40 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import * as S from "../style";
 import axios from "axios";
 import config from "../../../config";
 import Button from "../../default/button/Button";
 import Input from "../../default/input/Input";
 
-const Login = ({ setLoginMode, email, password, setEmail, setPassword }) => {
-  const onChangeEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const onChangePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const onSubmitLogin = (e) => {
-    e.preventDefault();
-    axios
-      .post(`${config.SERVER_URL}/auth`, { email, password })
-      .then((loginResult) => {
-        alert("로그인 성공");
-        localStorage.setItem("accessToken", loginResult.data.accessToken);
-        axios.defaults.headers.common["Authorization"] =
-          loginResult.data.accessToken;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
+const Login = ({
+  email,
+  password,
+  onChangeEmail,
+  onChangePassword,
+  onClickLoginModeAnchor,
+  onLogin,
+}) => {
   return (
     <S.AccountWrapper>
       <S.AccountTitleWrapper>
         <S.AccountTitle>로그인</S.AccountTitle>
       </S.AccountTitleWrapper>
-      <S.SubmitForm onSubmit={onSubmitLogin}>
+      <S.SubmitForm onSubmit={onLogin}>
         <S.AccountInputWrapper>
           <Input placeholder="이메일" value={email} onChange={onChangeEmail} />
           <Input
@@ -46,7 +30,7 @@ const Login = ({ setLoginMode, email, password, setEmail, setPassword }) => {
         </S.AccountInputWrapper>
         <Button value="로그인" />
       </S.SubmitForm>
-      <S.AccountModeChangeAnchor onClick={setLoginMode}>
+      <S.AccountModeChangeAnchor onClick={onClickLoginModeAnchor}>
         회원 가입 하시겠습니까?
       </S.AccountModeChangeAnchor>
     </S.AccountWrapper>
