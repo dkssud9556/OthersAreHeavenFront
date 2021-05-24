756e959771dab7b1d558e6c72c53df960fcf907c
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-24 23:34:50 +0900

    [UPDATE] use selector hooks

--- src/util/hooks/login/useLogin.jsx
+++ src/util/hooks/login/useLogin.jsx
@@ -1,19 +1,9 @@
 import { useDispatch } from "react-redux";
 import { useSelectState } from "../default";
-import { setLoginEmail, setLoginPassword } from "../../../modules/action/login";
 
 const useLogin = () => {
-  const dispatch = useDispatch();
   const state = useSelectState().login;
-  const setState = {
-    setLoginEmail: (payload) => {
-      dispatch(setLoginEmail(payload));
-    },
-    setLoginPassword: (payload) => {
-      dispatch(setLoginPassword(payload));
-    },
-  };
-  return { state, setState };
+  return { state };
 };
 
 export default useLogin;
--- src/util/hooks/main/useMain.jsx
+++ src/util/hooks/main/useMain.jsx
@@ -1,16 +1,9 @@
 import { useDispatch } from "react-redux";
 import { useSelectState } from "../default";
-import { setLoginMode } from "../../../modules/action/main";
 
 const useMain = () => {
-  const dispatch = useDispatch();
   const state = useSelectState().main;
-  const setState = {
-    setLoginMode: (payload) => () => {
-      dispatch(setLoginMode(payload));
-    },
-  };
-  return { state, setState };
+  return { state };
 };
 
 export default useMain;
