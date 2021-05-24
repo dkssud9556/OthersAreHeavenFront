de59769ddc5353fec53f12051d734194a25abb12
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-24 23:32:59 +0900

    [ADD] signup actions

--- src/modules/action/signup/index.js
+++ src/modules/action/signup/index.js
@@ -1,4 +1,10 @@
-import { SIGNUP_EMAIL, SIGNUP_PASSWORD } from "./interface";
+import {
+  SIGNUP,
+  SIGNUP_EMAIL,
+  SIGNUP_FAIL,
+  SIGNUP_PASSWORD,
+  SIGNUP_SUCCESS,
+} from "./interface";
 
 export const setSignupEmail = (payload) => ({
   type: SIGNUP_EMAIL,
@@ -10,4 +16,19 @@ export const setSignupPassword = (payload) => ({
   payload,
 });
 
-export { SIGNUP_EMAIL, SIGNUP_PASSWORD };
+export const signup = (payload) => ({
+  type: SIGNUP,
+  payload,
+});
+
+export const signupSuccess = (payload) => ({
+  type: SIGNUP_SUCCESS,
+  payload,
+});
+
+export const signupFail = (payload) => ({
+  type: SIGNUP_FAIL,
+  payload,
+});
+
+export { SIGNUP_EMAIL, SIGNUP_PASSWORD, SIGNUP, SIGNUP_SUCCESS, SIGNUP_FAIL };
--- src/modules/action/signup/interface/index.js
+++ src/modules/action/signup/interface/index.js
@@ -1,2 +1,5 @@
 export const SIGNUP_EMAIL = "signup/EMAIL";
 export const SIGNUP_PASSWORD = "signup/PASSWORD";
+export const SIGNUP = "signup/SIGNUP";
+export const SIGNUP_SUCCESS = "signup/SIGNUP_SUCCESS";
+export const SIGNUP_FAIL = "signup/SIGNUP_FAIL";
