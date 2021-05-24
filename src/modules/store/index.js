b8238e4b64de814bb798293512e495d388c3759e
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-24 23:34:27 +0900

    [UPDATE] store

--- src/modules/store/index.js
+++ src/modules/store/index.js
@@ -1,7 +1,29 @@
-import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
+import { configureStore } from "@reduxjs/toolkit";
+import { createBrowserHistory } from "history";
 import rootReducer from "../reducer";
+import rootSaga from "../saga";
 
-const store = createStore(rootReducer, composeWithDevTools());
+const customHistory = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware({
+  context: {
+    history: customHistory,
+  },
+});
+
+const createStore = () => {
+  const store = configureStore({
+    reducer: rootReducer,
+    devTools: true,
+    middleware: [sagaMiddleware],
+  });
+
+  sagaMiddleware.run(rootSaga);
+
+  return store;
+};
+
+const store = createStore();
 
 export default store;
