b0b1dc871207a4681875d5b4ede6e11d29479ba0
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-24 23:33:25 +0900

    [ADD] signup reducer

--- src/modules/reducer/signup/index.js
+++ src/modules/reducer/signup/index.js
@@ -1,4 +1,9 @@
 import { SIGNUP_EMAIL, SIGNUP_PASSWORD } from "../../action/signup";
+import {
+  SIGNUP,
+  SIGNUP_FAIL,
+  SIGNUP_SUCCESS,
+} from "../../action/signup/interface";
 
 const initState = {
   signupEmail: "",
@@ -7,18 +12,28 @@ const initState = {
 
 const SignUpReducer = (state = initState, action) => {
   switch (action.type) {
-    case SIGNUP_EMAIL: {
+    case SIGNUP_EMAIL:
       return {
         ...state,
         signupEmail: action.payload,
       };
-    }
-    case SIGNUP_PASSWORD: {
+    case SIGNUP_PASSWORD:
       return {
         ...state,
         signupPassword: action.payload,
       };
-    }
+    case SIGNUP:
+      return {
+        ...state,
+      };
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+      };
+    case SIGNUP_FAIL:
+      return {
+        ...state,
+      };
     default: {
       return state;
     }
