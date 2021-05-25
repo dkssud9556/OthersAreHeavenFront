56168de2f57d1f2cdb8db99ec08632ebebc8f531
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-25 23:34:00 +0900

    [UPDATE] yarn.lock

--- src/route/MainRouter.jsx
+++ src/route/MainRouter.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import MainContainer from "../containers/main/MainContainer";
-import Matching from "../components/matching/Matching";
+import MatchContainer from "../containers/match";
 
 const MainRouter = ({ match }) => {
   return (
     <>
       <Route exact={true} path={match.path} component={MainContainer} />
-      <Route path={`${match.path}/match`} component={Matching} />
+      <Route path={`${match.path}/match`} component={MatchContainer} />
     </>
   );
 };
