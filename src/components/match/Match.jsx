d32da41d33ef8fd78b3e1dfb9806ee7b60d7ce59
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-26 23:07:40 +0900

    [ADD] onStartMatch onClick method

--- src/components/match/Match.jsx
+++ src/components/match/Match.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Header from "./header";
 import * as S from "./style";
 
-const Match = () => {
+const Match = ({ onStartMatch }) => {
   return (
     <>
       <Header />
       <S.MatchingButtonWrapper>
-        <S.MatchingButton>
+        <S.MatchingButton onClick={onStartMatch}>
           <S.MatchingText>랜덤 매칭</S.MatchingText>
         </S.MatchingButton>
       </S.MatchingButtonWrapper>
--- src/containers/match/MatchContainer.jsx
+++ src/containers/match/MatchContainer.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import Match from "../../components/match/Match";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { startMatch } from "../../modules/action/match";
+import { connectSocket } from "../../lib/socket";
+
+const socket = connectSocket();
 
 const MatchContainer = () => {
-  return <Match />;
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const onStartMatch = () => {
+    dispatch(startMatch({ push: history.push, socket }));
+  };
+
+  return <Match onStartMatch={onStartMatch} />;
 };
 
 export default MatchContainer;
