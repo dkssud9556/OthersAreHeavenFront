2b62813a748babef2c9152ed5229527acc1bcb47
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-26 23:08:18 +0900

    [UPDATE] MainRouter.jsx

--- src/route/MainRouter.jsx
+++ src/route/MainRouter.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import { Route } from "react-router-dom";
 import MainContainer from "../containers/main/MainContainer";
 import MatchContainer from "../containers/match";
+import MatchingContainer from "../containers/matching";
 
 const MainRouter = ({ match }) => {
   return (
     <>
       <Route exact={true} path={match.path} component={MainContainer} />
-      <Route path={`${match.path}/match`} component={MatchContainer} />
+      <Route
+        exact={true}
+        path={`${match.path}/match`}
+        component={MatchContainer}
+      />
+      <Route
+        exact={true}
+        path={`${match.path}/matching`}
+        component={MatchingContainer}
+      />
     </>
   );
 };
