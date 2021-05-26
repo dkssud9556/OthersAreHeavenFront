95ed47956231c1e3c4c152c54fc44b98f9ddfb35
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-26 23:08:25 +0900

    [ADD] socket lib

---
+++ src/lib/socket.js
@@ -0,0 +1,8 @@
+import { io, Socket } from "socket.io-client";
+import config from "../config";
+
+export const connectSocket = () =>
+  io.connect(`${config.SERVER_URL}`, {
+    transports: ["websocket"],
+    path: "/socket.io",
+  });
