ad4cc29fbcd2e2dd1ce1e83c672c2fd5191f6ff3
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-26 23:06:34 +0900

    [ADD] Matching component

---
+++ src/components/matching/Matching.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Matching = () => {
+  return <div>매칭중...</div>;
+};
+
+export default Matching;
---
+++ src/containers/matching/MatchingContainer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Matching from "../../components/matching/Matching";
+
+const MatchingContainer = () => {
+  return <Matching />;
+};
+
+export default MatchingContainer;
