2bb96d0c1db397225eb70b210fe9f234d4662fce
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-28 12:06:11 +0900

    [UPDATE] socket code in MatchingContainer

--- src/containers/matching/MatchingContainer.jsx
+++ src/containers/matching/MatchingContainer.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Matching from "../../components/matching/Matching";
+import { connectSocket, emitAuthentication, emitMatch } from "../../lib/socket";
 
 const MatchingContainer = () => {
+  useEffect(() => {
+    const socket = connectSocket();
+    emitAuthentication(socket);
+    emitMatch(socket);
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return <Matching />;
 };
 
