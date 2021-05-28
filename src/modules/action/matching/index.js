e801a94c13b1c7322662ebcf6482e1a7c1ba82ec
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-28 12:06:43 +0900

    [ADD] start matching action

---
+++ src/modules/action/matching/index.js
@@ -0,0 +1,8 @@
+import { START_MATCHING } from "./interface";
+
+export const startMatching = (payload) => ({
+  type: START_MATCHING,
+  payload,
+});
+
+export { START_MATCHING };
---
+++ src/modules/action/matching/interface/index.js
@@ -0,0 +1,1 @@
+export const START_MATCHING = "match/START_MATCHING";
