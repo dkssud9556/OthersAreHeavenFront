061f0bbf235c7e77841059649f136ab4dda583f3
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-30 21:07:23 +0900

    [DELETE] match, matching reducers

--- src/modules/reducer/index.js
+++ src/modules/reducer/index.js
@@ -2,15 +2,11 @@ import { combineReducers } from "redux";
 import signUpReducer from "./signup";
 import mainReducer from "./main";
 import LoginReducer from "./login";
-import MatchReducer from "./match";
-import MatchingReducer from "./matching";
 
 const rootReducer = combineReducers({
   signup: signUpReducer,
   main: mainReducer,
   login: LoginReducer,
-  match: MatchReducer,
-  matching: MatchingReducer,
 });
 
 export default rootReducer;
