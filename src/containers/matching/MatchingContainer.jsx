e30e5b5f6c16484cb2e75f3d11913ba24528e2fa
김대웅 <dkssud9556@dsm.hs.kr>
2021-05-30 21:10:19 +0900

    [UPDATE] MatchingContainer

--- src/containers/matching/MatchingContainer.jsx
+++ src/containers/matching/MatchingContainer.jsx
@@ -1,19 +1,66 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Matching from "../../components/matching/Matching";
-import { connectSocket, emitAuthentication, emitMatch } from "../../lib/socket";
+import {
+  connectSocket,
+  emitAuthentication,
+  emitFindNewUser,
+  emitMatch,
+  emitNewMessage,
+  listenOnAuthenticated,
+  listenOnMatched,
+  listenOnReceiveMessage,
+} from "../../lib/socket";
+import { useHistory } from "react-router-dom";
 
 const MatchingContainer = () => {
+  const [isMatched, setIsMatched] = useState(false);
+  const [content, setContent] = useState("");
+  const [chats, setChats] = useState([]);
+  const socket = connectSocket();
+  const history = useHistory();
+
   useEffect(() => {
-    const socket = connectSocket();
+    listenOnMatched(socket, setIsMatched);
+    listenOnAuthenticated(socket);
+    listenOnReceiveMessage(socket, setChats);
     emitAuthentication(socket);
-    emitMatch(socket);
 
     return () => {
       socket.disconnect();
     };
   }, []);
 
-  return <Matching />;
+  const onSubmitChat = (e) => {
+    e.preventDefault();
+    emitNewMessage(socket, content);
+    setContent("");
+  };
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onClickStopMatching = () => {
+    history.push("/main/match");
+  };
+
+  const onClickFindNewUser = () => {
+    setIsMatched(false);
+    setChats([]);
+    emitFindNewUser(socket);
+  };
+
+  return (
+    <Matching
+      isMatched={isMatched}
+      chats={chats}
+      onSubmitChat={onSubmitChat}
+      content={content}
+      onChangeContent={onChangeContent}
+      onClickStopMatching={onClickStopMatching}
+      onClickFindNewUser={onClickFindNewUser}
+    />
+  );
 };
 
 export default MatchingContainer;
